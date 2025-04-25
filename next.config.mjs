37e5516a8b3ef0d4338310c82e91b5a04e9c56c9
Kerim Imamović <imamovic.kerim@gmail.com>
2025-04-25 15:08:25 +0200

    Next.js SQL Data Queries with Drizzle ORM

--- next.config.mjs
+++ next.config.mjs
@@ -1,49 +1,13 @@
 /** @type {import('next').NextConfig} */
 import { withSentryConfig } from "@sentry/nextjs";
-import path from 'path';
 
-const nextConfig = {
-  experimental: {
-    instrumentationHook: true,
-  },
+const nextConfig = {};
 
-  async redirects() {
-    const isProd = process.env.NODE_ENV === "production";
-
-    if (isProd) {
-      return [
-        {
-          source: "/sentry-example-page",
-          destination: "/",
-          permanent: false,
-        },
-        {
-          source: "/page",
-          destination: "/",
-          permanent: false,
-        },
-      ];
-    }
-
-    return [];
-  },
-
-  webpack(config) {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.resolve(__dirname, 'src'),
-    };
-    return config;
-  },
-};
-
-const sentryWebpackPluginOptions = {
-  org: "davegray",
-  project: "repairshop",
-  authToken: process.env.SENTRY_AUTH_TOKEN,
-  silent: false,
-  hideSourceMaps: true,
-  disableLogger: true,
-};
-
-export default withSentryConfig(nextConfig, sentryWebpackPluginOptions);
+export default withSentryConfig(nextConfig, {
+    org: "Full Stack Apps",
+    project: "repairshop",
+    authToken: process.env.SENTRY_AUTH_TOKEN,
+    silent: false, 
+    hideSourceMaps: true,
+    disableLogger: true,
+});
--- src/app/sentry-example-page/page.tsx
+++ src/app/sentry-example-page/page.tsx
@@ -1,26 +1,5 @@
-// app/dev-only/sentry-example-page/page.tsx
-"use client";
+"use client"
 
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-
-export default function DevOnlySentryExamplePage() {
-  const router = useRouter();
-
-  useEffect(() => {
-    if (process.env.NODE_ENV === "production") {
-      router.replace("/");
-    }
-  }, [router]);
-
-  if (process.env.NODE_ENV === "production") {
-    return null;
-  }
-
-  return (
-    <div>
-      <h1>Dev-only Sentry Page</h1>
-      <p>This page is only visible in development.</p>
-    </div>
-  );
+export default function Page() {
+  throw new Error("Sentry Test Frontend Error")
 }
