0d9add067243157f028dd9dff2d97723e1b4cf43
Kerim Imamović <imamovic.kerim@gmail.com>
2025-04-26 12:57:55 +0200

    Next.js with React-Hook-Form, Drizzle-Zod, & ShadCN/ui

--- src/app/(rs)/tickets/form/page.tsx
+++ src/app/(rs)/tickets/form/page.tsx
@@ -1,7 +1,8 @@
 import { getCustomer } from "@/lib/queries/getCustomer";
 import { getTicket } from "@/lib/queries/getTicket";
 import { BackButton } from "@/components/BackButton";
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from "@sentry/nextjs"
+import TicketForm from "./TicketForm";
 
 export default async function TicketFormPage({
   searchParams,
@@ -51,6 +52,7 @@ export default async function TicketFormPage({
 
       // return ticket form 
       console.log(customer)
+      return <TicketForm customer={customer} />
     }
 
     // Edit ticket form 
@@ -60,9 +62,7 @@ export default async function TicketFormPage({
       if (!ticket) {
         return (
           <>
-            <h2 className="text-2xl mb-2">
-              ID #{ticketId} not found
-            </h2>
+            <h2 className="text-2xl mb-2">Ticket ID #{ticketId} not found</h2>
             <BackButton title="Go Back" variant="default" />
           </>
         )
@@ -73,7 +73,7 @@ export default async function TicketFormPage({
       // return ticket form 
       console.log('ticket: ', ticket)
       console.log('customer: ', customer)
-
+      return <TicketForm customer={customer} ticket={ticket} />
     }
 
   } catch (e) {
