518f2cfd77dc3f257fb83206f3d8fe6e861aa2c1
Kerim Imamović <imamovic.kerim@gmail.com>
2025-04-30 10:49:52 +0200

    Next.js Server Actions with next-safe-action

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from '@/components/ui/toaster'
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -40,8 +41,9 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Toaster />
         </ThemeProvider>
       </body>
     </html>
   );
-}
+};
